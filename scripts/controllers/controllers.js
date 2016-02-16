740edf4950334cd439110eb4749714b005286d19
carlsim <similatron@gmail.com>
2016-02-15 22:59:06 -0500

    first commit

--- scripts/controllers/controllers.js
+++ scripts/controllers/controllers.js
@@ -2,7 +2,7 @@ var galleryControllers = angular.module('galleryControllers',[]);
 
 galleryControllers.controller('GalleryListCtrl', ['$scope', '$http', 
 	function($scope, $http){
-		$http.get('/photos/photos.json')
+		$http.get('https://raw.githubusercontent.com/carlsim/daily-ui-2016/gh-pages/photos/photos.json')
 		.success(function(data){
 			$scope.photos = data;
 		});
