b0ddf65263a58115062380bb1156e9ac4bfa797f
carlsim <similatron@gmail.com>
2016-02-15 22:57:37 -0500

    first commit

--- scripts/controllers/controllers.js
+++ scripts/controllers/controllers.js
@@ -6,19 +6,5 @@ galleryControllers.controller('GalleryListCtrl', ['$scope', '$http',
 		.success(function(data){
 			$scope.photos = data;
 		});
-	}]);
-
-galleryControllers.controller('GalleryDetailCtrl', ['$scope', '$routeParams', '$http',
-	function($scope, $routeParams, $http){
-		$http.get('/photos/' + $routeParams.photoId + '.json')
-		.success(function(data){
-			console.log($routeParams.photoId);
-			$scope.photo = data;
-		});
-		$http.get('/photos/' + $routeParams.photoId + '.json')
-		.success(function(data){
-			console.log($routeParams.photoId);
-			$scope.comments = data.comments;
-		});
-		$scope.upvotes = 10;
-	}]);
+	}
+]);
